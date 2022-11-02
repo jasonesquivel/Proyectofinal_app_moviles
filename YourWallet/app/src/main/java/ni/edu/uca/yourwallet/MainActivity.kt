0c7ae72fe9845e35d9affd349ca04e5a8061b5b7
jasonesquivel <95312146+jasonesquivel@users.noreply.github.com>
2022-11-01 23:34:40 -0600

    Error de navegacion resuelto utilizar esta sintaxis inicial para los fragmentos <?xml version="1.0" encoding="utf-8"?> <androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"     xmlns:app="http://schemas.android.com/apk/res-auto"     xmlns:tools="http://schemas.android.com/tools"     android:layout_width="match_parent"     android:layout_height="match_parent"     tools:context=".MainActivity">
        <androidx.fragment.app.FragmentContainerView
            android:id="@+id/nav_host_fragment"
            android:name="androidx.navigation.fragment.NavHostFragment"
            android:layout_width="match_parent"
            android:layout_height="match_parent"
            app:defaultNavHost="true"
            app:navGraph="@navigation/nav_graph" />
    
    </androidx.constraintlayout.widget.ConstraintLayout>

--- YourWallet/app/src/main/java/ni/edu/uca/yourwallet/MainActivity.kt
+++ YourWallet/app/src/main/java/ni/edu/uca/yourwallet/MainActivity.kt
@@ -2,13 +2,17 @@ package ni.edu.uca.yourwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import ni.edu.uca.yourwallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+            val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment)
     }
 }
